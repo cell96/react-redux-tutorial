8cbd0a89151e9128fa981a2ffd6f5bda14bc2065
Anees <anees.bajwa@mail.utoronto.ca>
2019-09-03 14:57:13 -0400

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+import Stories from './Stories';
+
+const App = ({ stories, onArchive }) =>
+    <div className="app">
+        <Stories />
+    </div>
+
+export default App;
